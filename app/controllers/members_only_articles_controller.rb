68419166f24cfc6f0a583faabee6562b3c6c4d1f
Wangari Samantha <samanthanyams5@gmail.com>
2023-07-03 08:16:18 +0300

    complete

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,4 +1,5 @@
 class MembersOnlyArticlesController < ApplicationController
+  before_action :require_login
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
@@ -12,6 +13,16 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   private
+
+  def require_login
+    unless logged_in?
+      render json: { error: 'Not authorized' }, status: :unauthorized 
+    end
+  end
+
+  def logged_in?
+    session[:user_id].present?
+  end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
